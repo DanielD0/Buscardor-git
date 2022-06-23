24ea18ec620f90011efd1bb55c5e57e53c8f3bf4
DanielD0 <37585619+DanielD0@users.noreply.github.com>
2022-06-23 17:49:40 -0500

    Proyecto sin diseño

---
+++ js/main.js
@@ -0,0 +1,83 @@
+const API = "https://api.github.com/users/";
+
+const requestMaxTimeMs = 3000;
+
+const app = Vue.createApp({
+    data(){
+        return{
+            search: null,
+            result: null,
+            error: null,
+            favorites: new Map()
+        };
+    },
+    created(){
+        const savedFavorites =JSON.parse(window.localStorage.getItem("favorites")); 
+        if(savedFavorites?.length){
+            const favorites = new Map(savedFavorites.map(favorite => [favorite.login, favorite]))
+            this.favorites = favorites
+        }
+    },
+    computed: {
+        isFavorite(){
+            return this.favorites.has(this.result.login)
+        },
+        allFavorites(){
+            return Array.from(this.favorites.values())
+        }
+
+    },
+    methods: {
+        async doSearch(){  
+            this.result = this.error = null
+            const founInFavorites = this.favorites.get(this.search)
+
+            const shoulRequest = (() =>{
+                if(!!founInFavorites){
+                    const {lastRequestTime} = founInFavorites
+                    const now = Date.now()
+                    return (now - lastRequestTime) > requestMaxTimeMs
+                }
+                return false
+            })()
+
+            if(!!founInFavorites && !shoulRequest){
+                console.log("Found and we use the cached version");
+                return this.result = founInFavorites
+            }
+
+            try {
+                console.log("not found or cached version is too old");
+                const response = await fetch(API + this.search)
+                if(!response.ok) throw new Error("User not found")
+                const data = await response.json()
+                this.result = data
+                founInFavorites.lastRequestTime = Date.now()
+            } catch (error) {
+                this.error = error
+            }finally{
+                this.search = null
+            }
+        },
+
+        addFavorite(){
+            this.result.lastRequestTime = Date.now()
+            this.favorites.set(this.result.login,this.result)
+            this.updateStorage()
+        },
+
+        removeFavorite(){
+            this.favorites.delete(this.result.login)
+            this.updateStorage()
+        },
+
+        showFavorite(favorite){
+            this.result = favorite
+        },
+
+        updateStorage(){
+            window.localStorage.setItem('favorites',JSON.stringify(this.allFavorites));
+        }
+
+    }
+});
